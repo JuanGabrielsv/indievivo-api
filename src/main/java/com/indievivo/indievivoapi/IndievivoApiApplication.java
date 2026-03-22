c072ee1dc0c2f3b68293938839c7720f5f9d9791
juangabrielsv <juangabrielsv@gmail.com>
2026-03-22 23:17:55 +0100

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/indievivo/indievivoapi/IndievivoApiApplication.java
@@ -0,0 +1,13 @@
+package com.indievivo.indievivoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndievivoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndievivoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indievivo/indievivoapi/IndievivoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indievivo.indievivoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndievivoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
